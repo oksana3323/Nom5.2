c9579e688204a99eb55da4b97c35a66e1abee4ee
oksana <oksana3323@gmail.com>
2023-02-11 13:08:39 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+
+public static double area (int a){
+    return Math.PI*(a*a);
+}
+
+public static double circumferemnce(int a){
+    return Math.PI*2*a;
+}
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+
+
+ int a=new Scanner(System.in).nextInt();
+        System.out.println(Circle.area(a));
+        System.out.println(Circle.area(a));
+
+        System.out.println(Circle.circumferemnce(a));
+    }
+}
